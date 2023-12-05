8e7a85377deebc2aa3d54bc00042ae4aea96129f
SridharBalakrishnan <sridharunicorn@gmail.com>
2023-12-05 13:53:22 +0000

    Update mongoDbConfig.js

--- config/mongoDbConfig.js
+++ config/mongoDbConfig.js
@@ -1,7 +1,7 @@
 // MongoDB Configurations :
 const mongodb = require('mongodb')
-//const dbUrl = 'mongodb://mybuddy:mybuddy123@13.233.23.16:27017/admin'
-const dbUrl = 'mongodb://mybyddyuat:mybuddyuat123@216.48.187.178:27017/mybuddyuat'
+const dbUrl = 'mongodb://mybuddy:mybuddy123@13.233.23.16:27017/admin'
+//const dbUrl = 'mongodb://mybyddyuat:mybuddyuat123@216.48.187.178:27017/mybuddyuat'
 // const dbUrl = 'mongodb+srv://Ganesh:Ganesh@mybuddyapp.tw3vhsn.mongodb.net/'
 const MongoClient = mongodb.MongoClient
 
